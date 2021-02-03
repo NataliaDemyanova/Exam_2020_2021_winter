471ecaa88d74e15fac70bf8cc7d295fcc15f3910
Natalia <61887532+NataliaDemyanova@users.noreply.github.com>
2021-02-03 12:34:42 +0300

    Add files via upload

--- 201_331_Demyanova.cpp
+++ 201_331_Demyanova.cpp
@@ -3,20 +3,31 @@
 #include <vector>
 #include <string>
 
+
+
+
 int main()
 {
-    int answ_task_1;
-    std::vector <bool> task_1 = { 0, 0, 1, 1 };
-    answ_task_1 = num_of_args(task_1);
-    std::cout << answ_task_1;
+	std::vector<bool> f = { 1 , 0, 1, 1, 0, 0 };
+	std::cout << num_of_args(f) << std::endl;
 
-    std::string read_file_name = "read_bool_vector.txt";
-    std::vector <bool> answ_task_2 = read_from_file(read_file_name);
-    for (auto data : answ_task_2) {
-        std::cout << data << ' ';
-    }
+	std::vector <bool> f1 = read_from_file("D:\\vector.txt");
+	int size = read_from_file("D:\\vector.txt").size();
+	for (int i = 0; i < size; i++) std::cout << f1[i] << std::endl;
 
-    std::string write_file_name = "";
+    std::string filePathOut("D:\\vector.txt");
+    printf_s("Writing vector to: %s\n", filePathOut.c_str());
+    bool result = write_to_file(filePathOut, f);
+    printf_s("Write result: %d (%s)\n", result, result ? "success" : "error");
+
+    std::string fTable = table(f);
+    printf_s("Table:\n%s\n", fTable.c_str());
+
+    printf_s("%s\n", func_sdnf(f).c_str());
+    printf_s("%s\n", func_sknf(f).c_str());
+
+
+
        
 
 
--- lab.cpp
+++ lab.cpp
@@ -4,69 +4,207 @@
 #include <string>
 #include <fstream>
 
+
 int num_of_args(std::vector<bool> f)
 {
-	int len = f.size();
-	int n;
-	while (len / 2 > 1) {
-		len /= 2;
-		n = len;
+	int size = f.size(), n = 0; // size - длина вектора функции, n - количество аргументов функции
+	while (size > 1) { //цикл, определяющий степень 2 числа
+		size /= 2;
+		n++;
 	}
 	return n;
 }
 
 std::vector<bool> read_from_file(std::string file_name)
 {
-	
-	std::ifstream file;
-	std::vector <bool> output;
-	char buff[16];
-	file.open(file_name);
-	file >> buff;
-	output.push_back(buff);
-	file.close();
-	return output;
+	std::vector <bool> f;
+	std::fstream fs;  //создание объекта при работе в С++
+	std::string str;
+	fs.open(file_name, std::fstream::in);  //открытие файла, режим: на чтение 
+	getline(fs, str); // считывание строки с вектором
+
+	for (int i = 0; i < str.length(); i += 2) {
+		f.push_back((bool)str[i]);
+		//cout << str[i] << endl;
+	}
+
+	fs.close();
+	return f;
 }
+
 
 bool write_to_file(std::string file_name, std::vector<bool> f)
 {
-	std::ofstream file;
-	file.open(file_name);
-	if (!file.is_open) {
-		return false;
-	}
-	for (auto data : f) {
-		file << data << ' ';
-	}
-	file.close();
-	return true;
+    std::ofstream out;
+
+    try
+{
+    out.open(file_name);
+    if (!out.good())
+        return false;
+
+    // Loop for each value in f vector
+    for (bool b : f)
+    {
+        // write in read-acceptible format (like as read_from_file)
+        out << b << " ";
+    }
+}
+catch (const std::exception& e)
+{
+    // Handle exception and return false (as declared in exam's task)
+    printf_s("Error while writing file\n%s\n", e.what());
+    return false;
+}
+
+// return true on successful read
+return true;
 }
 
 std::string table(std::vector<bool> f)
 {
-	std::string table;
+    std::string table;
+    int argNum = num_of_args(f);
 
-	int argNum = num_of_args(f);
+    // print table header
+    for (int i = 1; i <= argNum; i++)
+    {
+        table += "x" + std::to_string(i) + "\t";
+    }
 
-	// print table header
-	for (int i = 1; i <= argNum; i++)
-	{
-		table += "x" + std::to_string(i) + "\t";
-	}
+    table += "f\n";
 
-	return table;
+    // loop for all values of function 
+    for (int i = 0; i < f.size(); i++)
+    {
+        // loop for every row of table
+        for (int j = 0; j <= argNum; j++)
+        {
+            // if we're on function value, just print it to table
+            if (j == argNum)
+                table += std::to_string(f.at(i));
+            else
+                /*
+                Some serious bit math here!
+
+                Basically 'i' variable is a bit set of row of table
+                so we're masking it by current column that j var represents
+                to obtain useable mask, we need to substract 'j' from arg num
+                and pow 2 to its value
+                e.g.
+
+                if we're on 3rd row (i = 2), for 3 arguments, row would be 0 1 0
+                to obtain bool wariables, we need to bitmask 'i'. Mask would be
+                j = 0 : 1 0 0 | 2 & mask == 0
+                j = 1 : 0 1 0 | 2 & mask == 1
+                j = 2 : 0 0 1 | 2 & mask == 0
+                */
+                table += std::to_string((bool)(i & (int)(pow(2, (argNum - j - 1))))) + "\t";
+        }
+        table += "\n";
+    }
+
+    return table;
 }
-/*std::string func_sdnf(std::vector<bool> f)
+
+std::string func_sdnf(std::vector<bool> f)
 {
-    return string();
+    std::string func("f_sdnf(");
+    int argNum = num_of_args(f); // obtain number of arguments
+    bool hasSFunc = false; // used for handling if function has SDNF
+
+    // this loop prints function body, e.g.
+    // f_sdnf(x1, x2, x3) = 
+    for (int i = 1; i <= argNum; i++)
+    {
+        func += "x" + std::to_string(i);
+        if (i != argNum)
+            func += ", ";
+    }
+
+    // enclosure for body
+    func += ") = ";
+
+    // loop through all values of function
+    for (int i = 0; i < f.size(); i++)
+    {
+        // SDNF is constructed only for true values of function!
+        if (f.at(i))
+        {
+            // notify that function actually has sdnf
+            hasSFunc = true;
+            func += "(";
+
+            // loop through truth table
+            for (int j = 0; j < argNum; j++)
+            {
+                // same method of obtainig value for an argument used above
+                bool arg = (bool)(i & (int)(pow(2, (argNum - j - 1))));
+
+                // handle if argument's value is false
+                if (!arg)
+                    func += "'";
+
+                // just print element of formula
+                func += "x" + std::to_string(j + 1);
+                if (j != argNum - 1)
+                    func += " & ";
+
+            }
+
+            func += ") v ";
+        }
+    }
+
+    // trim 3 chars from the end of string for beaty
+    func.replace(func.length() - 3, 3, "");
+
+    return func;
 }
 
+// this function works just like as one above, but it's sknf
 std::string func_sknf(std::vector<bool> f)
 {
-    return string();
+    std::string func("f_sknf(");
+    int argNum = num_of_args(f);
+
+    for (int i = 1; i <= argNum; i++)
+    {
+        func += "x" + std::to_string(i);
+        if (i != argNum)
+            func += ", ";
+    }
+
+    func += ") = ";
+
+    for (int i = 0; i < f.size(); i++)
+    {
+        if (!f.at(i))
+        {
+            func += "(";
+
+            for (int j = 0; j < argNum; j++)
+            {
+                bool arg = (bool)(i & (int)(pow(2, (argNum - j - 1))));
+
+                if (arg)
+                    func += "'";
+
+                func += "x" + std::to_string(j + 1);
+                if (j != argNum - 1)
+                    func += " V ";
+            }
+
+            func += ") & ";
+        }
+    }
+
+    func.replace(func.length() - 3, 3, "");
+
+    return func;
 }
 
 std::string func_Zhegalkin(std::vector<bool> f)
 {
-    return string();
-}*/
+    return std::string();
+}
